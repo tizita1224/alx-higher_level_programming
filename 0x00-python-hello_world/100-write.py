e38ae99253716c92dc47294a348ece35b9c009d3
tizita <tizitabelachew80@gmail.com>
2024-01-02 14:32:58 -0800

    write

---
+++ 0x00-python-hello_world/100-write.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+import sys
+# Write the specified text to the standard error stream
+sys.stderr.write("and that piece of art is useful - Dora Korpar, 2015-10-19\n")
+# Exit with the status code 1
+sys.exit(1)
