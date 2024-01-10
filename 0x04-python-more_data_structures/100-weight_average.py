07d1bebba3ea322fe835c1b65d93a689d98bcf13
tizita <tizitabelachew80@gmail.com>
2024-01-10 12:50:15 -0800

    average

---
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+def weight_average(my_list=[]):
+    if not my_list:
+        return 0
+
+    numerator = 0
+    denominator = 0
+
+    for score, weight in my_list:
+        numerator += score * weight
+        denominator += weight
+
+    return numerator / denominator
