64133483fec64ab8cc2e043edba3c6ce2aec2484
root <tizita1224>
2024-05-23 15:17:52 -0700

    task 18

--- 0x0D-SQL_introduction/101-avg_temperatures.sql
+++ 0x0D-SQL_introduction/101-avg_temperatures.sql
@@ -1,2 +1,2 @@
 -- retrives average tempratures ordered by temprature
-SELECT city, AVG(tempratures) AS avg_temp from tempratures GROUP BY city  ORDER BY avg_temp;
+SELECT city, AVG(tempratures) AS avg_temp FROM tempratures GROUP BY city  ORDER BY avg_temp;
